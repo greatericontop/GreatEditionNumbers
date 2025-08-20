6a97527812f699922acfd37a4f5843dc9cc559a1
greatericontop <greateric@protonmail.com>
2025-08-20 19:24:59 -0400

    initial setup

---
+++ src/main/java/io/github/greatericontop/greateditionnumbers/CraftingListener.java
@@ -0,0 +1,4 @@
+package io.github.greatericontop.greateditionnumbers;
+
+public class CraftingListener {
+}
---
+++ src/main/java/io/github/greatericontop/greateditionnumbers/GreatEditionNumbers.java
@@ -0,0 +1,7 @@
+package io.github.greatericontop.greateditionnumbers;
+
+public class GreatEditionNumbers extends JavaPlugin {
+
+
+
+}
