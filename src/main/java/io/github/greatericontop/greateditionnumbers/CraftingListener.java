40d17e8ed476df333f6c2cb763a6cb4004a052fb
greatericontop <greateric@protonmail.com>
2025-08-20 19:53:10 -0400

    first try will it work

--- src/main/java/io/github/greatericontop/greateditionnumbers/CraftingListener.java
+++ src/main/java/io/github/greatericontop/greateditionnumbers/CraftingListener.java
@@ -1,4 +1,56 @@
 package io.github.greatericontop.greateditionnumbers;
 
-public class CraftingListener {
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CraftingListener implements Listener {
+
+    private final GreatEditionNumbers plugin;
+    public CraftingListener(GreatEditionNumbers plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST) // tries to run last to insert lore at the end in case any custom item plugins edit it
+    public void onCraft(CraftItemEvent event) {
+        String stringKey;
+        if (event.getRecipe() instanceof ShapedRecipe sr) {
+            stringKey = sr.getKey().toString();
+        } else if (event.getRecipe() instanceof ShapelessRecipe slr) {
+            stringKey = slr.getKey().toString();
+        } else {
+            return; // not a recipe we care about
+        }
+        if (!plugin.trackedRecipes.contains(stringKey))  return;
+        if (plugin.getConfig().getBoolean("skip-creative-players") && event.getWhoClicked().getGameMode() == GameMode.CREATIVE)  return;
+        String configLocation = String.format("_data_.%s", stringKey);
+        int count = plugin.getConfig().getInt(configLocation, 0) + 1;
+        ItemStack stack = event.getInventory().getResult();
+        if (stack == null) {
+            plugin.getLogger().warning(String.format("Result for craft (%s) is null, it probably shouldn't be!", stringKey));
+            return;
+        }
+        ItemMeta im = stack.getItemMeta();
+        if (im == null) {
+            plugin.getLogger().warning(String.format("Result for craft (%s) doesn't have an ItemMeta!", stringKey));
+            return;
+        }
+        List<String> lore = im.getLore();
+        if (lore == null) {
+            lore = new ArrayList<>();
+        }
+        lore.add(String.format("§7Edition #%s%,d", count == 1 ? "§6" : "§f", count));
+        stack.setItemMeta(im);
+        plugin.getConfig().set(configLocation, count);
+    }
+
 }
--- src/main/java/io/github/greatericontop/greateditionnumbers/GreatEditionNumbers.java
+++ src/main/java/io/github/greatericontop/greateditionnumbers/GreatEditionNumbers.java
@@ -1,7 +1,29 @@
 package io.github.greatericontop.greateditionnumbers;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
 public class GreatEditionNumbers extends JavaPlugin {
+    public List<String> trackedRecipes;
 
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+        trackedRecipes = this.getConfig().getStringList("tracked-recipes");
 
+        this.getServer().getPluginManager().registerEvents(new CraftingListener(this), this);
+
+        Bukkit.getScheduler().runTaskTimer(this, this::saveAll, 1200L, 1200L);
+
+        this.getLogger().info("GreatEditionNumbers finished setting up!");
+    }
+
+    private void saveAll() {
+        this.saveConfig();
+    }
 
 }
